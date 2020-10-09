dfce90efa7596cdd333be0f1713ecca69256f3e6
Fayaz Ahmed <fayazara@gmail.com>
2020-10-09 20:40:06 +0530

    First Commit

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center">
+    <button @click="checkout" class="bg-indigo-500 text-white px-4 py-2 text-xl rounded">Pay Now</button>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    checkout() {
+      const rzp_options = {
+        key: "rzp_test_o9cMykk3uCzB0C",
+        amount: 400,
+        currency: "INR",
+        name: "Testing",
+        description: "Testing Description",
+        handler: function (response) {
+          alert(`Payment Succesful ${response.razorpay_payment_id}`);
+        },
+        modal: {
+          ondismiss: function () {
+            alert(`Payment Failed`);
+          },
+        },
+        theme: {
+          color: "#667eea",
+        },
+      };
+      const rzp1 = new Razorpay(rzp_options);
+      console.log(rzp_options);
+      console.log(rzp1);
+      rzp1.open();
+    },
+  },
+};
+</script>
